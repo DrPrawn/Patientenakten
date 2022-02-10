27f58681a760af889627d881fbbb1848b40c0fd4
Benjamin Zimmer <benjamin.zimmer81@gmail.com>
2022-02-10 14:46:45 +0100

    Next:
    Button click event has to be defined

---
+++ Patientenakten/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Patientenakten
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Patientenakten/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace Patientenakten
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            string content = textBoxContent.Text;
+            string fileName = textBoxFileName.Text;
+
+            using (FileStream fs = File.Create(fileName))
+            {
+                byte[] contentConvert = Encoding.ASCII.GetBytes(content);
+                fs.Write(contentConvert, 0, contentConvert.Length);
+            }
+
+        }
+    }
+}
