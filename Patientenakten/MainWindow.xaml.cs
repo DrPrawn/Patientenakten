ec34171d502ec6b13f8cf1ef74b007ddc9da9636
Benjamin Zimmer <benjamin.zimmer81@gmail.com>
2022-02-14 15:36:13 +0100

    Unnützes Programm von dem Deppen aus diesem sinnlosen Kurs.
    Kann man ja noch zu Ende entwickeln.

--- Patientenakten/MainWindow.xaml.cs
+++ Patientenakten/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Text;
 using System.Windows;
 
@@ -10,6 +9,9 @@ namespace Patientenakten
     /// </summary>
     public partial class MainWindow : Window
     {
+        public const string DIR_PATH = @"C:\Users\abcd\Desktop\test\";
+        public const string FILE_EXT = ".txt";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -20,12 +22,26 @@ namespace Patientenakten
             string content = textBoxContent.Text;
             string fileName = textBoxFileName.Text;
 
-            using (FileStream fs = File.Create(fileName))
+            using (FileStream fs = File.Create(DIR_PATH + fileName + FILE_EXT))
             {
                 byte[] contentConvert = Encoding.ASCII.GetBytes(content);
                 fs.Write(contentConvert, 0, contentConvert.Length);
             }
+            MessageBox.Show("Datei wurde angelegt");
+        }
 
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            string fileName = textBoxFileName.Text;
+
+            using (FileStream fs = File.OpenRead(DIR_PATH + fileName + FILE_EXT))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string content = sr.ReadToEnd();
+                    textBoxContent.Text = content;
+                }
+            }
         }
     }
 }
